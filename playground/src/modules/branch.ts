cd257738eb6fbc6d21c46d4e918c05c33523bf1e
Rémi Alvergnat <toilal.dev@gmail.com>
2020-03-23 12:29:11 +0100

    feat(playground): Add interactive controls to discover git flow

--- playground/src/modules/branch.ts
+++ playground/src/modules/branch.ts
@@ -1,10 +1,5 @@
 import { Module, VuexModule } from 'vuex-module-decorators'
-import Branch from '@/models/branch'
 
 @Module
-export default class BranchModule extends VuexModule {
-  develop: Branch = new Branch('develop')
-  master: Branch = new Branch('master')
-
-  active: Branch | null = null
+export default class GitModule extends VuexModule {
 }
